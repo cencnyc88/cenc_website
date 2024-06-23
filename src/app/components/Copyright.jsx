3fc8f13798d8a38b50d9533e2829a1a5524591f2
Bob Lin <smallgiant1010@gmail.com>
2024-06-22 21:50:04 -0400

    Created Footer

---
+++ src/app/components/Copyright.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Copyright() {
+    return (
+        <div className="contact-container">
+            <div className="container-12">
+                <div className="contact-us">
+                    CONTACT US
+                </div>
+                <span className="address">
+                    Feel free to visit us @ 88 East Broadway New York, NY 10004 Unit #153 <br />
+
+                </span>
+                <div className="email">
+                    Feel free to email us @ cencnyc@gmail.com
+                </div>
+                <div className="container-8">
+                    <span className="copyright-label">
+                        Copyright © 2024 Chinatown East Neighboard Council. All Rights Reserved.
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
