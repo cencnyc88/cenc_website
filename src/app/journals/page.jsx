93875161b437caad072ef890851a9c08ee738068
Bob Lin <smallgiant1010@gmail.com>
2024-06-25 22:03:59 -0400

    PDF doesn't work

--- src/app/journals/page.jsx
+++ src/app/journals/page.jsx
@@ -4,7 +4,7 @@ export default function PDFDisplay() {
     return (
         <div className="pdf-display">
             <iframe
-                src="https://github.com/smallgiant1010/cenc_website/blob/b99155b7f75c2dfdc2b7d861f608f7591c2ae361/CENC%20JOURNAL%20(2)-compressed.pdf"
+                src="/CENC JOURNAL (2).pdf"
                 width="100%"
                 height="800px"
                 className="pdf-border"
